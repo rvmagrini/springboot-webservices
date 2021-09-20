635217a2c738bcffc7b055beae43e36a8dff4095
Renato Magrini <renatomagrini@gmail.com>
2021-09-20 13:35:37 +0200

    [1.1] [INIT] Created Spring Boot Project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# springboot-webservices
-Project for Spring Boot CRUD RESTful Web Services
+# Spring Boot WEB Services
+## Project for Spring Boot CRUD RESTful Web Services
+
+### Development Process:
+
+### 1: [INIT] Initializing
+TASKS:
+- [1.1] Create Spring Boot Project
---
+++ src/main/java/com/rvmagrini/springbootwebservices/SpringbootWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.rvmagrini.springbootwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebservicesApplication.class, args);
+	}
+
+}
